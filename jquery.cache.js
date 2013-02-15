e0a149426465dd91e09c4fd96e4cb56e662aaf5e
Joe Johnson <say2joe@gmail.com>
2013-02-15 15:36:20 -0800

    Added removal DOM event

--- jquery.cache.js
+++ jquery.cache.js
@@ -19,38 +19,61 @@
  * of the same selector lookups throughout your code, this functionality
  * will dramatically increase the performance of your web application.
  *
+ * NOTE: Remove objects from the cache by passing an empty string selector.
+ * NOTE: Do NOT bind events to nodes which may be removed from the document.
+ *
  * @param  {String} label		A label for the DOM node/collection to reference.
  * @param  {String} selector	(optional) A valid Sizzle/jQuery selector.
  * @return {jQuery Object}		A chainable jQuery object representing the DOM.
+ *
+ * If you are using jQuery < 2.x, domCache is the plugin name. If you prefer
+ * to use domCache as the name, remove the conditional var "name" assigment.
  */
 
 (function($){
 
-	var cache = {};
+	var cache = {},
+		extendMethods = {},
+		name = $.cache? "domCache" : "cache";
 
-	function updateElements() {
-		var coll = $(this.selector),
-			c = Math.max(this.length,coll.length);
-		while (c--) {
-			if (coll[c]) {
-				this[c] = coll[c];
-			} else if (this[c]) {
-				delete this[c];
+	$(document).on("DOMNodeRemoved",function(event){
+		var label, coll, l;
+		for (label in cache) {
+			l = (coll = cache[label]).length;
+			while (l--) {
+				if (coll[l] === event.target) {
+					delete coll[l]; --coll.length;
+				}
 			}
-		}
-		this.length = coll.length;
-		return this;
-	}
-
-	$.extend({
-		cache: function( label, selector ) {
-			if (selector) {
-				cache[label] = $(selector);
-				cache[label].selector = selector;
-				cache[label].refresh = updateElements;
-			}
-			return cache[label];
 		}
 	});
+
+	function updateElements() {
+		var cached = this,
+			jqo = $(cached.selector),
+			c = Math.max(cached.length,jqo.length);
+		while (c--) {
+			if (jqo[c]) {
+				cached[c] = jqo[c];
+			} else if (cached[c]) {
+				delete cached[c];
+			}
+		}
+		cached.length = jqo.length;
+		return cached;
+	}
+
+	extendMethods[name] = function( label, selector ) {
+		if (selector) {
+			cache[label] = $(selector);
+			cache[label].selector = selector;
+			cache[label].refresh = updateElements;
+		} else if (selector === '') {
+			delete cache[label];
+		}
+		return cache[label];
+	};
+
+	$.extend(extendMethods);
 
 })(jQuery);
